39231e1c0c33349cabd58bb835ea570cc08a8b03
mackica666 <149173442+mackica666@users.noreply.github.com>
2023-11-08 13:41:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omege tri i ostali kurčevi')
+streamlit.text('omege tri i ostali kurcevi')
